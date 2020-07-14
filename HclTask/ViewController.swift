0d3e948d52db3f8f65344533d706fbf04cbf3dee
Anand Sakthivel <sakthivelanand61@gmail.com>
2020-07-14 19:44:35 +0530

    Repository Created Universal APP

---
+++ HclTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HclTask
+//
+//  Created by Anand Sakthivel on 14/07/20.
+//  Copyright © 2020 Anand Sakthivel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
